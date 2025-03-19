2d63a7ca31ad55ac49e844b953719724d2fcb817
onairx <vnadipudi@outlook.com>
2025-03-19 23:10:52 +0530

    Fixed UI Errors

--- src/app/Components/Die.tsx
+++ src/app/Components/Die.tsx
@@ -11,7 +11,7 @@ export default function Die(props: DieProps) {
     return (
         <button
             className={`${props.isHeld ? "bg-green-300" : "bg-white"} shadow-lg rounded-2xl text-[#0a0a0a] 
-                text-4xl font-bold md:w-20 md:h-20 w-16 h-16 p-2`}
+                md:text-4xl text-3xl font-bold md:w-20 md:h-20 w-16 h-16 p-2`}
             onClick={() => props.flipClick(props.id)}
         >
             {props.value}
--- src/app/Components/Main.tsx
+++ src/app/Components/Main.tsx
@@ -4,7 +4,7 @@ import GameBox from "./GameBox";
 export default function Home() {
 
   return (
-    <div className="flex flex-col items-center p-5 h-screen w-screen text-center">
+    <div className="flex flex-col items-center md:p-5 p-2 h-screen w-screen text-center">
       <NavBar />
       <div className="w-full h-full flex justify-center items-center">
         <GameBox />
